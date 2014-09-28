fb19345ae8bdff860f0568d229bac58dbcbe9cb1
Ryan J. Geyer <me@ryangeyer.com>
2014-09-28 15:53:44 -0700

    Quantity updates

--- examples/quantity.cat.rb
+++ examples/quantity.cat.rb
@@ -11,7 +11,7 @@ end
 parameter 'method_param' do
   type 'string'
   label 'Method'
-  allowed_values 'array','clone'
+  allowed_values 'array','clone','multiprovision','raw'
 end
 
 resource 'base_server_res', type: 'server' do
@@ -56,19 +56,54 @@ define log($message, $notify) do
   rs.audit_entries.create(notify: $notify, audit_entry: {auditee_href: @@deployment.href, summary: $message})
 end
 
+define get_array_of_size($size) return $array do
+  $qty = 1
+  $qty_ary = []
+  while $qty <= to_n($size) do
+    $qty_ary << $qty
+    $qty = $qty + 1
+  end
+
+  $array = $qty_array
+end
+
 define launch(@base_server_res,@base_array_res,$qty_param,$method_param) return @base_server_res,@base_array_res do
+  call get_array_of_size($qty_param) retrieve $qty_ary
   if $method_param == 'array'
     provision(@base_array_res)
-  elsif $method_param == 'clone'
-    $qty = 1
-    $qty_ary = []
-    while $qty <= to_n($qty_param) do
-      $qty_ary << $qty
-      $qty = $qty + 1
-    end
+  end
 
+  if $method_param == 'multiprovision'
     concurrent foreach $qty in $qty_ary do
       provision(@base_server_res)
     end
   end
+
+  if $method_param == 'clone'
+    provision(@base_server_res)
+    concurrent foreach $qty in $qty_ary do
+      @new_res = @base_server_res.clone()
+      @new_res.update(name: 'Cloned #'+$qty)
+      # Change other things like inputs here
+      # Probably launch and wait too?
+    end
+  end
+
+  if $method_param == 'raw'
+    $params = {
+      'instance' => {
+        'cloud_href' => '/api/clouds/1',
+        'ssh_key_href' => '/api/clouds/1/ssh_keys/B393T34EO2K90',
+        'security_group_hrefs' => ['/api/clouds/1/security_groups/7OSUUQ36RMKOP'],
+        'server_template_href' => '/api/server_templates/341896004'
+      }
+    }
+    concurrent foreach $qty in $qty_ary do
+      $params['name'] = 'foo-'+to_s($qty)
+      # Change other things like inputs here
+      @resource_definition = {'namespace': 'rs', 'type': 'deployments', 'fields': $params}
+      provision(@resource_definition)
+    end
+  end
+  
 end
