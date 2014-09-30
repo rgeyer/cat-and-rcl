7a4c100fb402e2131befb96f800f39b2c741f8a8
Ryan J. Geyer <me@ryangeyer.com>
2014-09-29 17:17:41 -0700

    Found an issue with datetime arithmetic

---
+++ tests/datetime-arithmetic.cat.rb
@@ -0,0 +1,15 @@
+name 'datetime-arithmetic'
+rs_ca_ver 20131202
+short_description 'Arithmetic operations on datetime objects seems to break RCL completely'
+
+operation 'launch' do
+  description 'Do the stuff'
+  definition 'launch'
+end
+
+define launch() do
+  $one_hour_in_seconds = 3600
+  $created_at = to_d(10000)
+  $now = now()
+  $delta_native_type = $now - $created_at
+end
