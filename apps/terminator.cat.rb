e2fefdd2ead0b744862c495f14bb5da537dbc66d
Ryan J. Geyer <me@ryangeyer.com>
2014-09-30 16:37:41 -0700

    Terminator add a conditional to check for inactive servers since filters aren't passed to resource indexes (yet).  Also attempt to workaround concurrent map for a cloud resource collection

--- apps/terminator.cat.rb
+++ apps/terminator.cat.rb
@@ -26,10 +26,11 @@ end
 
 # From ../definitions/sys.cat.rb
 define get_clouds_by_rel($rel) return @clouds do
-  @clouds = concurrent map @cloud in rs.clouds.get() return @cloud_with_rel do
+  @@clouds = rs.clouds.empty()
+  concurrent foreach @cloud in rs.clouds.get() do
     $rels = select(@cloud.links, {'rel': $rel})
     if size($rels) > 0
-      @cloud_with_rel = @cloud
+      @@clouds = @@clouds + @cloud
     end
   end
 end
@@ -48,21 +49,24 @@ define terminator($instances_hours_old_param,$skip_tag_param) do
     sub task_name:'instances' do
       concurrent foreach @cloud in rs.clouds.get() do
         concurrent foreach @instance in @cloud.instances().get(filter: ['state<>inactive','state<>terminated']) do
-          $instances_hours_old_seconds = (to_n($instances_hours_old_param)*60)*60
-          call get_tags_for_resource(@instance) retrieve $tags
-          if type($tags) == 'null'
-            $tags = []
-          end
-          $created_at = to_n(@instance.created_at)
-          $created_delta = to_n(now()) - $created_at
+          # TODO: This if can go away once the filter above is actually applied
+          # Row 8 at - https://docs.google.com/a/rightscale.com/spreadsheets/d/1zEqFvhLDygFdxm588LGrHshpBgp41xvIeqjEVigdHto/edit#gid=0
+          if @instance.state != 'inactive' & @instance.state != 'terminated'
+            $instances_hours_old_seconds = (to_n($instances_hours_old_param)*60)*60
+            call get_tags_for_resource(@instance) retrieve $tags
+            if type($tags) == 'null'
+              $tags = []
+            end
+            $created_at = to_n(@instance.created_at)
+            $created_delta = to_n(now()) - $created_at
 
-          # TODO: This is comparing different data types, left hand is datetime, right hand is int/number
-          $is_old_enough = $created_delta > $instances_hours_old_seconds
-          $is_not_tagged = logic_not(contains?($tags, [$skip_tag_param]))
-          if $is_old_enough & $is_not_tagged
-            call log('Would terminate '+@instance.name+' because it is older than '+$instance_hours_old_seconds+' seconds, and is not tagged with'+$skip_tag_param,'None')
-          else
-            call log('Leaving '+@instance.name+' alone because it is not older than '+$instance_hours_old_seconds+' seconds, or is not tagged with'+$skip_tag_param,'None')
+            $is_old_enough = $created_delta > $instances_hours_old_seconds
+            $is_not_tagged = logic_not(contains?($tags, [$skip_tag_param]))
+            if $is_old_enough & $is_not_tagged
+              call log('Would terminate '+@instance.name+' because it is older than '+$instances_hours_old_seconds+' seconds, and is not tagged with '+$skip_tag_param,'None')
+            else
+              call log('Leaving '+@instance.name+' alone because it is not older than '+$instances_hours_old_seconds+' seconds, or is not tagged with '+$skip_tag_param,'None')
+            end
           end
         end
       end
@@ -73,7 +77,7 @@ define terminator($instances_hours_old_param,$skip_tag_param) do
     end
 
     sub task_name:'snapshots' do
-      call get_clouds_by_rel('volume_snapshots') retrieve @clouds
+      #call get_clouds_by_rel('volume_snapshots') retrieve @clouds
     end
 
     sub task_name:'ips' do
