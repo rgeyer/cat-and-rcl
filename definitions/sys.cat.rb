1a021d2b1291a0e475c709f8a551a4eb3efafba7
Ryan J. Geyer <me@ryangeyer.com>
2014-10-30 09:05:23 -0700

    More documentation

--- definitions/sys.cat.rb
+++ definitions/sys.cat.rb
@@ -1,3 +1,9 @@
+# Creates a simple array of the specified size.  The array contains integers
+# indexed from 1 up to the specified size
+#
+# @param $size [int] the desired number of elements in the returned array
+#
+# @return [Array] a 1 indexed array of the specified size
 define get_array_of_size($size) return $array do
   $qty = 1
   $qty_ary = []
@@ -9,27 +15,51 @@ define get_array_of_size($size) return $array do
   $array = $qty_ary
 end
 
+# Logs a message in the "summary" field of an audit entry (limited to 255 char)
+# with the CloudApp deployment as the auditee_href
+#
+# @param $message [String] the message to write in the "summary" field of an audit entry
+# @param $notify [String] the event notification category, one of (None|Notification|Security|Error)
+#
+# @see http://reference.rightscale.com/api1.5/resources/ResourceAuditEntries.html#create
 define log($message, $notify) do
   rs.audit_entries.create(notify: $notify, audit_entry: {auditee_href: @@deployment.href, summary: $message})
 end
 
+# Logs a message as an audit entry with the CloudApp deployment as the auditee_href
+#
+# @param $summary [String] the value to write in the "summary" field of an audit entry
+# @param $details [String] the message to write in the "detail" field of an audit entry
+# @param $notify [String] the event notification category, one of (None|Notification|Security|Error)
+#
+# @see http://reference.rightscale.com/api1.5/resources/ResourceAuditEntries.html#create
 define log_with_details($summary, $details, $notify) do
   rs.audit_entries.create(notify: $notify, audit_entry: {auditee_href: @@deployment.href, summary: $summary, detail: $details})
 end
 
+# Returns a resource collection containing clouds which have the specified relationship.
+#
+# @param $rel [String] the name of the relationship to filter on.  See cloud
+#   media type for a full list
+#
+# @return [CloudResourceCollection] The clouds which have the specified relationship
+#
+# @see http://reference.rightscale.com/api1.5/media_types/MediaTypeCloud.html
 define get_clouds_by_rel($rel) return @clouds do
-  @@clouds = rs.clouds.empty()
-  concurrent foreach @cloud in rs.clouds.get() do
+  @clouds = concurrent map @cloud in rs.clouds.get() return @cloud_with_rel do
     $rels = select(@cloud.links, {"rel": $rel})
     if size($rels) > 0
-      @@clouds = @@clouds + @cloud
+      @cloud_with_rel = @cloud
     end
   end
-  @clouds = @@clouds
 end
 
+# Fetches the execution id of "this" cloud app using the default tags set on a
+# deployment created by SS.
+# selfservice:href=/api/manager/projects/12345/executions/54354bd284adb8871600200e
+#
+# @return [String] The execution ID of the current cloud app
 define get_execution_id() return $execution_id do
-  #selfservice:href=/api/manager/projects/12345/executions/54354bd284adb8871600200e
   call get_tags_for_resource(@@deployment) retrieve $tags_on_deployment
   $href_tag = concurrent map $current_tag in $tags_on_deployment return $tag do
     if $current_tag =~ "(selfservice:href)"
