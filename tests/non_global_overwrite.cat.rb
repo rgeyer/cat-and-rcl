51e28176fe704f6b89ff9c852d0d8f366d6fc679
Ryan J. Geyer <me@ryangeyer.com>
2014-10-30 16:10:05 -0700

    non global scope issues

---
+++ tests/non_global_overwrite.cat.rb
@@ -0,0 +1,25 @@
+name "non_global_overwrite"
+rs_ca_ver 20131202
+short_description "This is not an empty string"
+
+operation "launch" do
+  description "Do the stuff"
+  definition "launch"
+end
+
+define foo() do
+  $not_global = {foo: "foo"}
+
+  call baz()
+  if $not_global != {foo: "foo"}
+    raise "baz() overwrite a non global hash defined/declared in foo().  Expected $not_global to be {foo: foo} but it was "+to_s($not_global)
+  end
+end
+
+define baz() do
+  $not_global = {baz: "baz"}
+end
+
+define launch() do
+  call foo()
+end
