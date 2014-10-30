c788d54f1ee3bccf1515159d646cd323d8f6b54c
Ryan J. Geyer <me@ryangeyer.com>
2014-10-30 14:13:07 -0700

    Added type resource collection test

---
+++ tests/type_does_not_report_resource_in_collection.cat.rb
@@ -0,0 +1,16 @@
+name "type_does_not_report_resource_in_collection"
+rs_ca_ver 20131202
+short_description "This is not an empty string"
+
+operation "launch" do
+  description "Do the stuff"
+  definition "launch"
+end
+
+define launch() do
+  @server_collection = rs.servers.empty()
+  $type = type(@server_collection)
+  if $type != "rs.servers"
+    raise "Expected type() to return \"rs.servers\" for a server collection, got \""+$type+"\" instead."
+  end
+end
