633b56778cc1a5f05c719313570d503541b4c94d
Ryan J. Geyer <me@ryangeyer.com>
2014-10-30 18:36:55 -0700

    more description

--- tests/map_collection_return_always_server.cat.rb
+++ tests/map_collection_return_always_server.cat.rb
@@ -16,3 +16,28 @@ define launch() do
     end
   end
 end
+
+# This results in
+# launch failed:
+#
+# Problem:
+#   Resource collection type mismatch
+# Origin:
+#   line: 26, column: 34
+#     task: /root
+#     expression:
+#     $revisions, @head_rev_script = concurrent map @script in @scripts return $return_revision, @return_script do
+#     |   $return_revision = @script.revision
+#     |   if to_s(@script.revision) == "0"
+#     |   |   @return_script = @script
+#     |   end
+#     end
+# Summary:
+#   Cannot concatenate a resource collection of rs.right_scripts to a resource collection of rs.servers (types must match)
+# Resolution:
+#   Make sure resource collection types match
+
+
+# If I remove the if statement from the map, such that every iteration assigns
+# something to @return_script, the error goes away.  I'm not sure what causes it
+# but it is reproducible
