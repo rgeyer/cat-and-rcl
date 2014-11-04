25f5693fd0a311b63423d5418386ef0fd64499ff
Ryan J. Geyer <me@ryangeyer.com>
2014-11-04 09:43:12 -0800

    Update for UTF-8 test

---
+++ tests/passing/utf-8.cat.rb
@@ -0,0 +1,10 @@
+name "UTF-8 Test"
+rs_ca_ver 20131202
+short_description "Caché"
+
+parameter "foo" do
+  type "string"
+  label "Foo"
+  default "Caché"
+  operations "launch"
+end
--- tests/utf-8.cat.rb
+++
@@ -1,3 +0,0 @@
-name "UTF-8 Test"
-rs_ca_ver 20131202
-short_description "Caché"
