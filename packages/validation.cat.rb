e1915fb7d1979c1810653081c273ecd8519afe9b
Ryan J. Geyer <me@ryangeyer.com>
2016-10-20 12:57:44 -0700

    Name conflict fix and documentation for validation package

--- packages/validation.cat.rb
+++ packages/validation.cat.rb
@@ -4,6 +4,16 @@ package "validation"
 short_description "A set of helper functions for validating user inputs and raising exceptions."
 import "exception"
 
+# Validates that a resource collection contains precisely the number of items
+# specified.
+#
+# @param @resource [ResourceCollection] the resource collection to validate
+# @param $desired_size [Int] the expected number of resources in the collection
+# @param $origin [String] the name of the package and function asking for
+#   validation. Used in the raised exception.
+#
+# @raise exception.invalidArgument() when the resource collection does not
+#   contain the desired number of resources.
 define resource_collection_size(@resource, $desired_size, $origin) do
   if size(@resource) != $desired_size
     $message = "Resource collection size, expected "+$desired_size+" items but got "+size(@resource)
@@ -11,10 +21,19 @@ define resource_collection_size(@resource, $desired_size, $origin) do
   end
 end
 
-define resource_collection_type(@resource, $type, $origin) do
+# Validates that a resource collection contains the correct type of resource
+#
+# @param @resource [ResourceCollection] the resource collection to validate
+# @param $desired_type [String] the expected type of resources in the collection
+# @param $origin [String] the name of the package and function asking for
+#   validation. Used in the raised exception.
+#
+# @raise exception.invalidArgument() when the resource collection does not
+#   contain the desired type of resources.
+define resource_collection_type(@resource, $desired_type, $origin) do
   $type = to_s(@resource)
-  if !($type =~ $type)
-    $message = "Resource collection type, expected rs_cm.instances "+$type+" but got "+$type
+  if !($type =~ $desired_type)
+    $message = "Resource collection type, expected rs_cm.instances "+$desired_type+" but got "+$type
     call exception.invalidArgument($origin, $message)
   end
 end
